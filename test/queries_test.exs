00e0a1eab9aee93ea81b591d309580292a6a6b84
jhchabran <jh@chabran.fr>
2017-10-13 17:54:01 +0200

    Remove queries and add exercise summary

--- test/queries_test.exs
+++ test/queries_test.exs
@@ -1,49 +1,70 @@
 defmodule FishingSpot.QueriesTest do
   use ExUnit.Case, async: true
-  import ExUnit.CaptureIO
+  import Ecto.Query
   ExUnit.configure(trace: true)
 
-  alias FishingSpot.Queries
   alias FishingSpot.Repo
+  alias FishingSpot.Location
+  alias FishingSpot.LocationTrip
+  alias FishingSpot.LocationType
+  alias FishingSpot.Fisherman
+  alias FishingSpot.FishermanTrip
   alias FishingSpot.FishLanded
+  alias FishingSpot.FishSpecies
+  alias FishingSpot.FlyType
+  alias FishingSpot.Trip
+  alias FishingSpot.Account
+
+  alias FishingSpot.Query
 
   setup do
    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
-   Repo.insert(%FishLanded{length: Decimal.new(1.0)})
-   :ok
   end
 
-  queries =  [
-    &Queries.all_fishermen/0,
-    &Queries.ordered_fishermen/0,
-    &Queries.biggest_fish/0,
-    &Queries.fishy_fish/0,
-    &Queries.fisherman_by_keyword/0,
-    &Queries.fishy_fish_count/0,
-    &Queries.biggest_fish_per_fisherman/0,
-    &Queries.biggest_fish_per_fisherman_ordered/0,
-    &Queries.biggest_fish_per_fisherman_in_clause/0,
-    &Queries.biggest_fish_per_fisherman_not_in_clause/0,
-    &Queries.biggest_fish_per_fisherman_two_queries/0,
-    &Queries.biggest_fish_catcher/0,
-    &Queries.biggest_fish_catcher_in_clause/0,
-    &Queries.biggest_fish_details/0,
-    &Queries.complex_select_fragment/0,
-    &Queries.fish_per_day/0,
-    &Queries.composed_biggest_fish_details/0,
-    &Queries.prefixed/0,
-    &Queries.distinct_fish_weight/0,
-    &Queries.distinct_fish_weight_with_expression/0,
-    &Queries.distinct_fish_weight_with_order_by/0,
-    &Queries.biggest_fish_per_fisherman_having/0,
-    &Queries.all_fish_limit_10/0,
-    &Queries.all_fish_limit_10_offset_half/0,
-    &Queries.biggest_fish_catcher_join/0,
-  ]
+  # See schema diagram here: https://gist.githubusercontent.com/parkerl/b25e56ee9a76e93be0b0/raw/3351336524cbd57bff1d6ab04d6a0a85beb5e5e8/gistfile1.txt
 
-  Enum.each(queries, fn(query)->
-    test "testing #{inspect(query)}" do
-      capture_io(unquote(query))
-    end
-  end)
+  test "find all fishermen" do
+    fishermen = false # FILL ME, Repo.all ...
+    assert 8 = length(fishermen)
+  end
+
+  test "select a tuple of name and date_of_birth for all fishermen" do
+    fishermen_names_and_dates = false # FILL ME, Repo.all ...
+    assert(
+      Enum.reduce(fishermen_names_and_dates, true, fn
+        x, acc when is_tuple(x) -> acc && true
+        x, acc -> acc && false
+      end),
+      "result is not a list of tuples"
+    )
+  end
+
+
+  test "order fishermen by name" do
+    fishermen = false # FILL ME, Repo.all ...
+    assert "Joe" = hd(fishermen)
+  end
+
+  test "find length of the biggest fish" do
+    biggest_fish_length = false # FILL ME, Repo.all ...
+    assert Decimal.new(90) == biggest_fish_length
+  end
+
+  test "find the two fishermen named Lew, born on 1976/01/05" do
+    {_, date} = Ecto.Date.cast("1976-01-05")
+    name      = "Lew"
+
+    fishermen = #false # FILL ME, Repo.all
+    Repo.all(
+      from fisherman in Fisherman,
+      where: [name: "Lew", date_of_birth: ^date]
+    )
+
+    assert [%Fisherman{id: 4}, %Fisherman{id: 8}] = fishermen
+  end
+
+  test "find the biggest fish catcher" do
+    fisherman = false # FILL ME, Repo.one ...
+    assert {"Kirk", _90_0} = fisherman
+  end
 end
